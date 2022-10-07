71d0c30a06deaad2770082119e9abc477dc372c4
Galal Mohamed <galalmgy@gmail.com>
2022-10-07 20:41:53 +0200

    modified:   udagram/set_env.sh 	modified:   udagram/udagram-api/package.json

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -8,4 +8,4 @@ export AWS_BUCKET=arn:aws:s3:::galal-udagram
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
-export URL=http://http://udagram-env.us-east-1.elasticbeanstalk.com
+export URL=http://udagram-env.us-east-1.elasticbeanstalk.com
