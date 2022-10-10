821981f070a9c874fb8d994d6e93f6604e2e5a89
Galal Mohamed <galalmgy@gmail.com>
2022-10-10 19:14:42 +0200

    modified:   udagram/udagram-api/src/config/config.ts

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -17,7 +17,7 @@ export const config = {
   aws_media_bucket: process.env.AWS_BUCKET,
   aws_access_key_id:process.env.AWS_ACCESS_KEY_ID,
   aws_secret_access_key: process.env.AWS_SECRET_ACCESS_KEY,
-  url: `${process.env.URL}`,
+  url: process.env.URL,
   jwt: {
     secret: process.env.JWT_SECRET,
   },
